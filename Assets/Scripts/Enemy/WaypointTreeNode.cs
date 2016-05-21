8e3b51fef4121f268e02c937d17dd737b76c4d60
LarcekGiglamesh <klein_mk@web.de>
2016-05-21 16:36:57 +0200

    - Added TreeNodes to Waypoints for Dynamic Path Finding - Further Prepared Prototype Scene for Enemy AI

---
+++ Assets/Scripts/Enemy/WaypointTreeNode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointTreeNode : MonoBehaviour
+{
+    public bool m_HasSubWaypoints;
+    public GameObject[] m_SubWayPoints;
+
+    public string m_UniqueName;
+    public GameObject[] m_NearbyWaypoints;
+
+    void Start()
+    {
+        m_UniqueName = gameObject.GetHashCode().ToString();
+    }
+}

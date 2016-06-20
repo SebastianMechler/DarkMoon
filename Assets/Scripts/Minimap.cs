2518cd84cf8a8ae3fc12eba9e65ac6b44649a984
SebastianMechler <sebastian.mechler@hotmail.de>
2016-06-20 10:48:04 +0200

    - Implemented basic "Minimap" - There will be Minimap-Markers to highlight objects in Minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour
+{
+  public Transform target;
+	void Start ()
+  {
+	
+	}
+	
+	void LateUpdate()
+  {
+    this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, target.transform.position.z);
+	}
+}

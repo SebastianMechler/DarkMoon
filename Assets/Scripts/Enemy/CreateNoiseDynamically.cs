c6c39b2416f492361b20f9b80c7aefcc380e941c
SebastianMechler <sebastian.mechler@hotmail.de>
2016-07-08 12:03:59 +0200

    - Items can now make noise for enemy

---
+++ Assets/Scripts/Enemy/CreateNoiseDynamically.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateNoiseDynamically : MonoBehaviour
+{
+
+  public GameObject m_Enemy;
+  public string m_UniqueName;
+  //public bool m_PlaySoundOnEnter;
+  private GameObject m_NearestWaypoint;
+
+  float lastNearestDistance = float.MaxValue;
+
+  public string getName()
+  {
+    return m_UniqueName;
+  }
+
+  public GameObject getNearestWaypoint()
+  {
+    return m_NearestWaypoint;
+  }
+
+  public void UpdateNearestWaypoint()
+  {
+    m_UniqueName = gameObject.GetHashCode().ToString();
+
+    GameObject[] list = GameObject.FindGameObjectsWithTag(StringManager.Tags.Waypoints);
+    Vector3 thisGameObject = gameObject.transform.position;
+    Vector3 next;
+
+    int size = list.Length;
+    for (int i = 0; i < size; i++)
+    {
+      next = list[i].transform.position;
+      float distance = Vector3.Distance(thisGameObject, next);
+      if (distance < lastNearestDistance)
+      {
+        lastNearestDistance = distance;
+        m_NearestWaypoint = list[i];
+      }
+    }
+
+    Debug.Log("Nearest waypoint dynamically set to: " + m_NearestWaypoint);
+  }
+
+  public void MakeNoiseAtCurrentPosition()
+  {
+    UpdateNearestWaypoint();
+    m_Enemy.GetComponent<EnemyAiScript>().changeMovementPattern(EnemyAiScript.MovementPattern.STATIC, gameObject, m_NearestWaypoint);
+  }
+
+  public void OnCollisionEnter(Collision other)
+  {
+    MakeNoiseAtCurrentPosition();
+  }
+}
--- Assets/Scripts/Inventory/ThrowItem.cs
+++ Assets/Scripts/Inventory/ThrowItem.cs
@@ -4,17 +4,37 @@ using System.Collections;
 public class ThrowItem : MonoBehaviour
 {
   Rigidbody m_rigidbody;
+  bool m_isFlying = false;
 
   void Start()
   {
     m_rigidbody = this.gameObject.GetComponent<Rigidbody>();
   }
 
+  void Update()
+  {
+    if (m_isFlying)
+    {
+      if (m_rigidbody.velocity == Vector3.zero)
+      {
+        // play sound
+        //SingletonManager.AudioManager.Play(AudioType.PUZZLE_FAILURE);
+        m_isFlying = false;
+      }
+      //Debug.Log("VELOCITY: " + m_rigidbody.velocity);
+    }
+  }
+
 	public void Throw(float itemForce)
   {
     Debug.Log("Throwing item...");
-
+    m_isFlying = true;
     Vector3 forward = Camera.main.transform.forward;
-    m_rigidbody.AddForce(new Vector3(forward.x * itemForce, 0,  forward.z * itemForce), ForceMode.Impulse);
+    m_rigidbody.AddForce(new Vector3(forward.x * itemForce, forward.y * itemForce,  forward.z * itemForce), ForceMode.Impulse);
+  }
+
+  public bool IsFlying()
+  {
+    return m_isFlying;
   }
 }

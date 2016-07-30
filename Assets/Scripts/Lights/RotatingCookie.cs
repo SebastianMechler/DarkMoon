d379f9d2c1b2d31cfcdb8ada6cfe6a06eda42f96
LarcekGiglamesh <klein_mk@web.de>
2016-07-30 17:50:27 +0200

    - Added some Simple Cookie Spotlight - Cookie Spotlight has RotatingCookie.cs, rotating by given factor

---
+++ Assets/Scripts/Lights/RotatingCookie.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingCookie : MonoBehaviour
+{
+    public float m_Speed = 8.0f;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Rotate(Vector3.forward, m_Speed * Time.deltaTime);
+    }
+}
